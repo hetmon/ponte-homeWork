30247a8f7eb57fcd23bcc4b58aef624a8601884f
tamas <tamasduro@gmail.com>
2025-01-11 09:05:12 +0100

    added cors mapping to config

--- src/main/java/hu/ponte/homework/pontevotehomework/config/SpringWebConfig.java
+++ src/main/java/hu/ponte/homework/pontevotehomework/config/SpringWebConfig.java
@@ -5,13 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("hu.ponte.homework.pontevotehomework")
-public class SpringWebConfig {
+public class SpringWebConfig implements WebMvcConfigurer {
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
 
     @Bean
     public MessageSource messageSource() {
@@ -20,5 +31,7 @@ public class SpringWebConfig {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+
+
 
 }
